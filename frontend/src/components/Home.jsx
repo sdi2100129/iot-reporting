1e4855f8761ca590bca2a20e44a9d7607c911b88
aorfanoudaki <aorfanou@netcompany.com>
2026-02-02 17:00:27 +0200

    home page almost done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,23 +2,24 @@ import bg from "../assets/layered-steps-haikei.svg";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col justify-center items-center min-h-[calc(100vh-5rem)] text-black px-6 text-center overflow-hidden">
-    
-    {/* Background SVG */}
-    <img 
-        src={bg} 
-        alt="Background pattern" 
-        className="absolute inset-0 w-full h-full object-cover -z-10 animate-bg"
-    />
+    <div className="relative z-0 w-screen left-1/2 right-1/2 -ml-[50vw] -mr-[50vw] flex flex-col justify-center items-center min-h-[calc(100vh-5rem)] pt-px text-white px-6 text-center overflow-hidden">
 
-    <h1 className="text-5xl md:text-6xl font-bold mb-6 mt-4">
+      {/* Background SVG */}
+      <img
+        src={bg}
+        alt="Background pattern"
+        className="absolute inset-0 w-full h-full -z-10 object-cover pointer-events-none"
+      />
+
+      {/* Content */}
+      <h1 className="text-5xl md:text-6xl font-bold mb-6 mt-4 z-10">
         Sensor Management Platform
-    </h1>
+      </h1>
 
-    <p className="text-slate-400 max-w-2xl text-lg">
+      <p className="text-white max-w-2xl text-lg z-10">
         Monitor, manage and analyze your IoT sensors in one powerful dashboard.
         Built for performance, clarity and real-time control.
-    </p>
+      </p>
     </div>
-  )
+  );
 }
--- frontend/src/components/Sensors/SensorForm.jsx
+++ frontend/src/components/Sensors/SensorForm.jsx
@@ -14,7 +14,7 @@ export default function SensorForm({newSensors, setNewSensors, addSensor, update
       <h3 className="text-6xl md:text-7xl font-extrabold text-purple-600 mb-8 tracking-wider font-[cursive]">
         Add Sensor
       </h3>
-      
+
       {newSensors.map((sensor, index) => (
         <div key={index} >
           {fields.map(field => (
@@ -25,6 +25,10 @@ export default function SensorForm({newSensors, setNewSensors, addSensor, update
               onChange={e => handleInputChange(index, field, e.target.value)}
             />
           ))}
+
+
+
+          
           <br />
           <button onClick={() => addSensor(sensor)}>Add</button>
           <button onClick={() => updateSensor(sensor)}>Update</button>
